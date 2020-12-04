25a500e5fcb54c6be00aae2472887ee516018f92
Ivan Georgiev <ivan.georgiev@nl.abnamro.com>
2020-12-04 14:12:33 +0100

    initial version

--- src/pyaz/cli/cli.py
+++ src/pyaz/cli/cli.py
@@ -1,25 +1,33 @@
 import os
+import io
+import re
 import sys
+import time
 import json
 import csv
 import click
 import dotenv
 from . import command as cmd
 
-def json_printer(indent=None):
+def json_printer(indent=None, print_to_file=None):
     def json_print(doc):
-        print(json.dumps(doc, indent=indent))
+        if print_to_file:
+            with open(print_to_file, "w", encoding="utf-8") as f:
+                f.write(json.dumps(doc, indent=indent))
+        else:
+            print(json.dumps(doc, indent=indent))
     return json_print
 
 _printer = None
 
 @click.group()
 @click.option('--indent', '-i', type=int, help='Output indent')
-def cli(indent):
+@click.option('--print-to-file', '-f', type=str, help='Print output to a file')
+def cli(indent, print_to_file):
     global _printer
 
     dotenv.load_dotenv()
-    _printer = json_printer(indent=indent)
+    _printer = json_printer(indent=indent, print_to_file=print_to_file)
 
 @cli.group(name='helper')
 def cli_helper():
@@ -27,16 +35,24 @@ def cli_helper():
 
 @cli_helper.command(name='json-to-csv', help='Transform JSON file to CSV')
 @click.option('--input-file', '-i', help='Input file name')
+@click.option('--output-file', '-o', help='Output file name')
 @click.option('--mapping', '-m', required=True, help='Mapping file name')
-def helper_json_to_csv(input_file, mapping):
+def helper_json_to_csv(input_file, output_file, mapping):
     if input_file:
         with open(input_file, 'r') as f:
             input = f.read()
     else:
         input = sys.stdin.read()
+    buffer = io.StringIO()
     result = cmd.helper_transform(input, mapping)
-    writer = csv.writer(sys.stdout)
+    writer = csv.writer(buffer)
     writer.writerows(result)
+    lines = [ line for line in re.split('[\\r\\n]', buffer.getvalue()) if line ]
+    f = open(output_file, 'w', encoding='utf-8') if output_file else sys.stdout
+    for line in lines:
+        print(line, file=f)
+    if (f is not sys.stdout): 
+        f.close()
 
 @cli.group(name='devops')
 @click.option('--pat-token', '-t', help='Azure DevOps personal access token (PAT).')
@@ -93,3 +109,94 @@ def az_devops_test_get_test_run_by_id(*args, **kwargs):
     result = cmd.test_get_test_run_by_id(*args, **kwargs)
     _printer(result)
 
+
+@az_devops.group(name="git")
+def az_devops_git():
+    cmd.init_git()
+
+@az_devops_git.command(name='list')
+@click.option('--project', '-p', help='Azure DevOps project name.')
+@click.option('--include-links', is_flag=True)
+@click.option('--include-hidden', is_flag=True)
+@click.option('--include-all-urls', is_flag=True)
+def az_devops_git_list(*args, **kwargs):
+    global _printer
+
+    result = cmd.git_get_repositories(*args, **kwargs)
+    _printer(result)
+
+@az_devops_git.group(name="commit")
+def az_devops_git_commit():
+    cmd.init_git()
+
+
+@az_devops_git_commit.command(name='list', help="List commits in a git repository")
+@click.option('--project', '-p', help='Azure DevOps project name.')
+@click.option('--repository-id', '-i', help="Repository ID")
+@click.option('--add-repository-id', '-a', is_flag=True, help="If specified, repository_id will be added to each commit")
+@click.option('--top', '-t', default=100, type=int, help="Time to wait, ")
+@click.option('--relax-interval', '-i', default=2.0, type=float, help="Time to wait, ")
+def az_devops_git_commit_list(*args, **kwargs):
+    global _printer
+
+    if kwargs.get('repository_id', None):
+        repository_id_list = [kwargs['repository_id']]
+        relax_interval = 0.0
+    else:
+        repositories_result = cmd.git_get_repositories()
+        repository_id_list = [repo_info['id'] for repo_info in repositories_result]
+        relax_interval = float(kwargs.get('relax_interval', 2))
+
+    result = []
+    params = {
+        'top': kwargs['top']
+    }
+
+    is_first = True
+    for repository_id in repository_id_list:
+        if not is_first:
+            time.sleep(relax_interval)
+            is_first = False
+        params['repository_id'] = repository_id
+        repository_result = cmd.git_commit_list(**params)
+        if kwargs.get('add_repository_id', False):
+            for i in range(len(repository_result)):
+                repository_result[i]['repository_id'] = repository_id
+        result.extend(repository_result)
+    _printer(result)
+
+
+@az_devops.group(name="pipeline")
+def az_devops_pipeline():
+    cmd.init_pipeline()
+
+@az_devops_pipeline.command(name='list')
+@click.option('--project', '-p', help='Azure DevOps project name.')
+# @click.option('--include-links', is_flag=True)
+# @click.option('--include-hidden', is_flag=True)
+# @click.option('--include-all-urls', is_flag=True)
+def az_devops_pipeline_list(*args, **kwargs):
+    global _printer
+
+    result = cmd.pipeline_list_pipelines(*args, **kwargs)
+    _printer(result)
+
+
+@az_devops_pipeline.command(name='get')
+@click.option('--project', '-p', help='Azure DevOps project name.')
+@click.option('--pipeline-id', '-i', help='Pipeline ID')
+def az_devops_pipeline_list(*args, **kwargs):
+    global _printer
+
+    result = cmd.pipeline_get_pipeline(*args, **kwargs)
+    _printer(result)
+
+@az_devops_pipeline.command(name='list-runs')
+@click.option('--project', '-p', help='Azure DevOps project name.')
+@click.option('--pipeline-id', '-i', help='Pipeline ID')
+@click.option('--relax-interval', '-i', default=2.0, type=float, help="Time to wait, ")
+def az_devops_pipeline_list(project, pipeline_id, relax_interval):
+    global _printer
+
+    result = cmd.pipeline_list_runs(project, pipeline_id, relax_interval=relax_interval)
+    _printer(result)
--- src/pyaz/cli/command.py
+++ src/pyaz/cli/command.py
@@ -1,35 +1,61 @@
 import json
+import datetime
+import time
+import io
+from types import SimpleNamespace
 from collections.abc import Iterable
 from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 from ..helper.etree import as_etree, as_table
 
+class DefaultSimpleNamespace(SimpleNamespace):
+    def __getattribute__(self, attr):
+        try:
+            return super().__getattribute__(attr)
+        except AttributeError:
+            return None
+
+
 _connection = None
 _project = None
 _build_client = None
 _test_client = None
+_git_client = None
+_pipeline_client = None
 
 def connect(pat, organization, project):
     global _connection, _project
     organization_url = 'https://dev.azure.com/{ORGANIZATION_NAME}'.format(ORGANIZATION_NAME=organization)
     credentials = BasicAuthentication('', pat)
     _connection = Connection(base_url=organization_url, creds=credentials)
+    _connection._config.verify=False
     _project = project
 
 def init_build():
     global _build_client, _connection
+
+    _connection._config.verify=False
     _build_client = _connection.clients_v6_0.get_build_client()
-
-
-
+    _build_client.config.verify=False
+    
 def make_json_friendly(doc):
-    if isinstance(doc, Iterable):
+    if isinstance(doc, str):
+        return doc
+    if isinstance(doc, datetime.datetime):
+        return doc.isoformat()
+    if isinstance(doc, Iterable) and not isinstance(doc, dict):
         return [make_json_friendly(i) for i in doc]
     else:
+        if hasattr(doc, '__dict__'):
+            result = { k: make_json_friendly(v) for k,v in doc.__dict__.items() }
+        else:
+            result = doc
+        return result
         return doc.as_dict()
 
 def build_get_builds():
     global _build_client, _project
+    _build_client.config.connection.verify = False
     build_list = _build_client.get_builds(_project)
     result = make_json_friendly(build_list)
     return result
@@ -82,4 +108,87 @@ def helper_transform(input, mapping_filename):
         output_fields=mapping.get('output_fields', None)
     )
     result = as_table(doc,**mapping_options)
+    return result
+
+def init_git():
+    global _git_client, _connection
+    _git_client = _connection.clients_v6_0.get_git_client()
+
+
+def git_get_repositories(*args, **kwargs):
+    global _git_client, _project
+
+    if len(args) < 1 and not kwargs.get('project',None):
+        kwargs['project'] = _project
+    raw_result = _git_client.get_repositories(*args, **kwargs)
+    result = make_json_friendly(raw_result)
+    return result
+
+def git_commit_list(repository_id, project=None, top=100):
+    global _git_client, _project
+
+    search_criteria = DefaultSimpleNamespace()
+    search_criteria.top = top
+    
+    params = {
+        'project': project or _project,
+        'repository_id': repository_id,
+        'search_criteria': search_criteria
+    }
+    raw_result = _git_client.get_commits(**params)
+    result = make_json_friendly(raw_result)
+    return result
+
+
+
+def init_pipeline():
+    global _pipeline_client, _connection
+    _pipeline_client = _connection.clients_v6_0.get_pipelines_client()
+
+
+def pipeline_list_pipelines(*args, **kwargs):
+    global _pipeline_client, _project
+
+    if len(args) < 1 and not kwargs.get('project',None):
+        kwargs['project'] = _project
+    raw_result = _pipeline_client.list_pipelines(*args, **kwargs)
+    result = make_json_friendly(raw_result)
+    return result
+
+def pipeline_get_pipeline(*args, **kwargs):
+    global _pipeline_client, _project
+
+    if len(args) < 1 and not kwargs.get('project',None):
+        kwargs['project'] = _project
+    
+    if (kwargs.get('pipeline_id',None)):
+        raw_result = _pipeline_client.get_pipeline(*args, **kwargs)
+        # print(raw_result._links)
+        # return ({})
+    else:
+        if 'pipeline_id' in kwargs:
+            del(kwargs['pipeline_id'])
+        raw_result = []
+        for pipeline in _pipeline_client.list_pipelines(*args, **kwargs):
+            kwargs['pipeline_id'] = pipeline.id
+            pipeline = _pipeline_client.get_pipeline(*args, **kwargs)
+            raw_result.append(pipeline)
+            time.sleep(2)
+    result = make_json_friendly(raw_result)
+    return result
+
+def pipeline_list_runs(project, pipeline_id, relax_interval=0):
+    global _pipeline_client, _project
+
+    project = project or _project
+    
+    if pipeline_id:
+        raw_result = _pipeline_client.list_runs(project, pipeline_id)
+    else:
+        raw_result = []
+        for pipeline in _pipeline_client.list_pipelines(project):
+            pipeline_runs = _pipeline_client.list_runs(project, pipeline.id)
+            raw_result.extend(pipeline_runs)
+            time.sleep(relax_interval)
+    result = make_json_friendly(raw_result)
     return result
